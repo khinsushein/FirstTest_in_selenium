fe4a0dcad7aaa65600adb6c213b8256d12082d1f
Khin <115156852+khinsushein@users.noreply.github.com>
2022-10-10 03:22:26 +0200

    Update Test01.py

--- Test01.py
+++ Test01.py
@@ -1,10 +1,14 @@
-#open the "https://top-movies-qhyuvdwmzt.now.sh/"
+#open the "https://top-movies-qhyuvdwmzt.now.sh/" in Chrome
 from selenium import webdriver
-from selenium.webdriver.common.keys import Keys
-driver = webdriver.Chrome('./chromedriver.exe')
- 
+#from selenium.webdriver.common.keys import keys
+driver=webdriver.Chrome(executable_path="C:\Drivers\chromedriver_win32\chromedriver.exe")
+# open in Chrome browser
 driver.get("https://top-movies-qhyuvdwmzt.now.sh/")
-main_window = browser.current_window_handle
+# Title of the page
 print(driver.title)
-
+print(driver.current_url)
+driver.find_element_by_xpath("//*[@id='Tabbed']/a/button").click()
+time.sleep(5)
+#close the browser
+driver.close()
 
