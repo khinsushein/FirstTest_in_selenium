0922587c566d4c8abbfd35f7bd2dd6b3ad9d0eb6
Khin <115156852+khinsushein@users.noreply.github.com>
2022-10-10 03:54:38 +0200

    Update Test05_1.py

--- Test05_1.py
+++ Test05_1.py
@@ -1,42 +1,34 @@
-import pytest
-
-from selenium.webdriver import Chrome
+# Search for A New and verify that all the titles displayed contain the search phrase
+from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
-import selenium.webdriver as webdriver
-driver = webdriver.Chrome('./chromedriver.exe')
-import selenium.webdriver.support.ui as ui
-from selenium.webdriver.common.keys import Keys
+driver = webdriver.Chrome(executable_path="C:\Drivers\chromedriver_win32\chromedriver.exe")
 from time import sleep
 import org.openqa.selenium.WebElement
 import Xpath
 from selenium.common.exceptions import NoSuchElementException
 
 
-
-
-
-
 def test_basic_search(browser):
   # Set up some test case data
   URL = 'https://top-movies-qhyuvdwmzt.now.sh'
-  PHRASE2 = 'Star Trek:'
-  bodyText = self.driver.find_element_by_tag_name('PHRASE2').text
+  PHRASE3 = 'God the Father'
+  bodyText = self.driver.find_element_by_tag_name('PHRASE3').text
 
-  # Navigate to the The Shawshank Redemption home page
+  # Navigate to the 'God the Father'page
   browser.get(URL)
 
   # Find the search input element
   search_input = browser.find_element_by_id('search_form_input_homepage')
 
   # Send a search phrase to the input and hit the RETURN key
-  search_input.send_keys(PHRASE2 + Keys.RETURN)
+  search_input.send_keys(PHRASE3 + Keys.RETURN)
 
-  # Search "Star Trek: First Contact"on the current page
+  # Search "God the Father"on the current page
   driver.implicitly_wait(20)
 
 
 #identify element
-l= driver.find_elements_by_css_selector("The Shawshank Redemption")
+l= driver.find_elements_by_css_selector("God the Father")
 #get list size with len
 s = len(l)
 # check condition, if list size > 0, element exists
